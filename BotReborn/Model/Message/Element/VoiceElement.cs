7fd0ac6ce62080de5d6b710b1dcf9f49af5dd7e9
baka-gourd <fanng233@gmail.com>
2021-07-12 19:28:54 +0800

    u

--- BotReborn/Model/Message/Element/VoiceElement.cs
+++ BotReborn/Model/Message/Element/VoiceElement.cs
@@ -1,5 +1,12 @@
 ﻿namespace BotReborn.Model.Message.Element {
     public class VoiceElement {
-        
+        public string Name { get; set; }
+        public byte[] Md5 { get; set; }
+        public int Size { get; set; }
+        public string Url { get; set; }
+        /// <summary>
+        /// 在发送时使用
+        /// </summary>
+        public byte[] Data { get; set; }
     }
 }
---
+++ BotReborn/Model/VipInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace BotReborn.Model {
+    public class VipInfo {
+        public long Uin { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public float LevelSpeed { get; set; }
+        public string VipLevel { get; set; }
+        public int VipGrowthSpeed { get; set; }
+        public int VipGrowthTotal { get; set; }
+    }
+}
