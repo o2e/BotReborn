d15d4eea96240b75671b963d80b1b75cefb5076d
summpot <Summpot@outlook.com>
2021-07-15 22:39:43 +0800

    add sg

---
+++ BotReborn.SourceGen/JcePacketGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace BotReborn.SourceGen
+{
+    [Generator]
+    class JcePacketGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+        }
+        public void Initialize(GeneratorInitializationContext context) => throw new NotImplementedException();
+    }
+}
