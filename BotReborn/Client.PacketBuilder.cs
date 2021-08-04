8cdae0e068cc504f21ffe0941c2e7704618f7803
baka-gourd <fanng233@gmail.com>
2021-08-04 17:50:35 +0800

    update

---
+++ BotReborn/Client.PacketBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotReborn
+{
+    partial class Client
+    {
+        public (ushort, byte[]) BuildLoginPacket()
+        {
+            var seq = NextSeq();
+            throw new NotImplementedException();
+        }
+
+        public (ushort, byte[]) BuildDeviceLockLoginPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort, byte[]) BuildQrCodeFetchRequestPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort, byte[]) BuildQrCodeResultQueryRequestPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort, byte[]) BuildQrCodeLoginPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort, byte[]) BuildCaptchaPacket()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
