b55f364e74717f43ac9f30015a46a8367836ded5
summpot <Summpot@outlook.com>
2021-08-05 22:11:52 +0800

    Add sample

---
+++ BotReborn.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BotReborn.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uin = int.Parse((Environment.GetEnvironmentVariable("BotAccount", EnvironmentVariableTarget.User) ?? Environment.GetEnvironmentVariable("BotAccount"))!);
+            var passwd = Environment.GetEnvironmentVariable("BotPassword", EnvironmentVariableTarget.User) ?? Environment.GetEnvironmentVariable("BotPassword");
+            var client = new OicqClient(uin, passwd);
+            client.Login();
+        }
+    }
+}
