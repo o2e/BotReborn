2609b18117b3ce261623c7103c807e3ed4c1d4a3
summpot <Summpot@outlook.com>
2021-08-05 17:19:16 +0800

    Add empty decoders

---
+++ BotReborn/Packets/IncomingPacketInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotReborn.Packets
+{
+    class IncomingPacketInfo
+    {
+        public string CommandName;
+        public ushort SequenceId;
+        public Dictionary<string, object> Params;
+    }
+}
