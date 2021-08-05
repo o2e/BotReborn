b5957ea49d8c8151180c31ee5acb28a8dc8bc2d3
baka-gourd <fanng233@gmail.com>
2021-08-05 12:08:59 +0800

    update

--- BotReborn/Tlv.cs
+++ BotReborn/Tlv.cs
@@ -66,5 +66,53 @@ namespace BotReborn
 
             return binaryStream.ToArray();
         }
+
+        public static byte[] T1F() => throw new NotImplementedException();
+        public static byte[] T2() => throw new NotImplementedException();
+        public static byte[] T8() => throw new NotImplementedException();
+        public static byte[] T10A() => throw new NotImplementedException();
+        public static byte[] T16() => throw new NotImplementedException();
+        public static byte[] T16A() => throw new NotImplementedException();
+        public static byte[] T16E() => throw new NotImplementedException();
+        public static byte[] T17A() => throw new NotImplementedException();
+        public static byte[] T17C() => throw new NotImplementedException();
+        public static byte[] T18() => throw new NotImplementedException();
+        public static byte[] T33() => throw new NotImplementedException();
+        public static byte[] T35() => throw new NotImplementedException();
+        public static byte[] T52D() => throw new NotImplementedException();
+        public static byte[] T100() => throw new NotImplementedException();
+        public static byte[] T104() => throw new NotImplementedException();
+        public static byte[] T106() => throw new NotImplementedException();
+        public static byte[] T107() => throw new NotImplementedException();
+        public static byte[] T108() => throw new NotImplementedException();
+        public static byte[] T109() => throw new NotImplementedException();
+        public static byte[] T116() => throw new NotImplementedException();
+        public static byte[] T124() => throw new NotImplementedException();
+        public static byte[] T128() => throw new NotImplementedException();
+        public static byte[] T141() => throw new NotImplementedException();
+        public static byte[] T142() => throw new NotImplementedException();
+        public static byte[] T143() => throw new NotImplementedException();
+        public static byte[] T144() => throw new NotImplementedException();
+        public static byte[] T145() => throw new NotImplementedException();
+        public static byte[] T147() => throw new NotImplementedException();
+        public static byte[] T154() => throw new NotImplementedException();
+        public static byte[] T166() => throw new NotImplementedException();
+        public static byte[] T174() => throw new NotImplementedException();
+        public static byte[] T177() => throw new NotImplementedException();
+        public static byte[] T187() => throw new NotImplementedException();
+        public static byte[] T188() => throw new NotImplementedException();
+        public static byte[] T191() => throw new NotImplementedException();
+        public static byte[] T193() => throw new NotImplementedException();
+        public static byte[] T194() => throw new NotImplementedException();
+        public static byte[] T197() => throw new NotImplementedException();
+        public static byte[] T198() => throw new NotImplementedException();
+        public static byte[] T202() => throw new NotImplementedException();
+        public static byte[] T400() => throw new NotImplementedException();
+        public static byte[] T401() => throw new NotImplementedException();
+        public static byte[] T511() => throw new NotImplementedException();
+        public static byte[] T516() => throw new NotImplementedException();
+        public static byte[] T521() => throw new NotImplementedException();
+        public static byte[] T525() => throw new NotImplementedException();
+        public static byte[] T536() => throw new NotImplementedException();
     }
 }
